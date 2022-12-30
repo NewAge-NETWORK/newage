dfc7a4f9df896a367780debd123c66600a3ad8a9
NewAge-NETWORK <121613592+NewAge-NETWORK@users.noreply.github.com>
2022-12-30 18:49:11 +0100

    Website upload
    - Task done by Karlo

---
+++ main/js/index.js
@@ -0,0 +1,9 @@
+console.log("JavaScript loaded.")
+console.log("Found index.html"); 
+console.log("Found style.css"); 
+
+console.clear(); 
+
+if("index.html" = "true", () => {
+    console.log("JavaScript loaded.")
+}); 
